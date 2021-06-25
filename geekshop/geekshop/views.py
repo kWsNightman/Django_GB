b68f3bd9ed1fe801b2cfd325d84ea923bd3bfc6b
Alex Miruk <kws.nightmen@gmail.com>
2021-06-25 03:08:59 +0300

    Lesson_2_Completed

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'магазин'
+    }
+    return render(request, 'index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'contact.html')
+    context = {
+        'title': 'контакты'
+    }
+    return render(request, 'contact.html', context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
